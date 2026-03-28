4ebbc060ca08edfa903b767907c849c3e5b03327
Zhixin <zhixin.zhu99@gmail.com>
2026-03-28 09:47:08 +0100

    remove Link import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Journal from "./pages/Journal"
 import Header from "./layout/Header"
